7961563ff0d20aeb59f3a083bcb8736b3a873a44
xPeach-94 <96383489+xPeach-94@users.noreply.github.com>
2022-07-28 16:06:21 +0200

    slideshow update
    multiple slideshow update

--- Scripts/Modules/Module_moreSlideshows.js
+++ Scripts/Modules/Module_moreSlideshows.js
@@ -15,14 +15,23 @@ let slide2ImgSrc = [
     "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B980%2C853%5D&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F34%2F2021%2F10%2F06%2Fpeacock-displayed-feathers-getty-1021-2000.jpg"
 ];
 
-let imgSrcs = [slide1ImgSrc, slide2ImgSrc];
+let slide3ImgSrc = [
+    "https://cdn.britannica.com/37/154237-050-A76A506D/blue-peafowl-tail-Indian-peacock-courtship-displays.jpg",
+    "https://site-547756.mozfiles.com/files/547756/medium/peacock.jpg",
+    "https://i.guim.co.uk/img/media/80f866674f2a6d03dfbbcddff7475aeec325951e/0_158_2000_1200/master/2000.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=dd55996fbafe3383299e0022f04e7ed9",
+    "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B980%2C853%5D&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F34%2F2021%2F10%2F06%2Fpeacock-displayed-feathers-getty-1021-2000.jpg",
+    "https://dutchreview.com/wp-content/uploads/image00001-1-e1597223439731.jpeg",
+    "https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/1:1/w_2000,h_2000,c_limit/gettyimages-1146431497.jpg"
+];
 
-let slideShowHeight = ["30vh", "50vh"]
+let imgSrcs = [slide1ImgSrc, slide2ImgSrc, slide3ImgSrc];
 
-let dot = [true, true];
+let slideShowHeight = ["30vh", "50vh", "40vh"]
+
+let useDots = [true, true, true];
 
 
-const createSlideshowElements = function(index, height, dots)
+const createSlideshowElements = function(index, height, useDots)
 {
     let slideshow = document.getElementById("slideshow" + index);
 
@@ -65,7 +74,7 @@ const createSlideshowElements = function(index, height, dots)
     slideshow.appendChild(slideshowContainer);
 
 
-    if (dots) 
+    if (useDots) 
     {
         // Dots
 
@@ -90,7 +99,7 @@ for (let i = 1; i <= 100; i++)
 {
     if (document.getElementById("slideshow" + i) != null) 
     {
-        createSlideshowElements(i, slideShowHeight[i-1], dot[i-1]);
+        createSlideshowElements(i, slideShowHeight[i-1], useDots[i-1], slideshowI = i);
     }
     else
     {
@@ -101,10 +110,22 @@ for (let i = 1; i <= 100; i++)
 // Functionality
 
 let slideIndex = [1,1]; // slide number, slide index
-let slideId = ["slides1", "slides2"]
+let slideId = []
 
-showSlides(1, 0);
-showSlides(1, 1);
+for (let i = 1; i <= imgSrcs.length; i++) 
+{
+    slideId.push("slides"+i);
+}
+
+for (let i = 0; i <= imgSrcs.length; i++) 
+{
+    showSlides(1, i);
+    console.log(i);
+}
+
+// showSlides(1, 0);
+// showSlides(1, 1);
+// showSlides(1, 2);
 
 function plusSlides(slideNumber, slideshowIndex) 
 {
@@ -113,29 +134,34 @@ function plusSlides(slideNumber, slideshowIndex)
 
 function currentSlide(slideNumber, slideshowIndex) 
 {
-    console.log("slideNumber", slideNumber, "slideshowIndex,", slideshowIndex);
-
-    // showSlides(slideIndex[slideshowIndex] += slideNumber, slideshowIndex);
+    showSlides(slideIndex[slideshowIndex] = slideNumber, slideshowIndex);
 }
 
 function showSlides(slideNumber, slideshowIndex) 
 {
-    console.log(slideId, slideshowIndex, slideId[slideshowIndex]);
-
     let i;
     let x = document.getElementsByClassName(slideId[slideshowIndex]);
-    let dots = document.getElementsByClassName("dot");
-    if (slideNumber > x.length) {slideIndex[slideshowIndex] = 1}    
-    if (slideNumber < 1) {slideIndex[slideshowIndex] = x.length}
+    
+    
+    if (slideNumber > x.length) 
+    {
+        slideIndex[slideshowIndex] = 1
+    }    
+    if (slideNumber < 1) 
+    {
+        console.log(slideNumber);
+        slideIndex[slideshowIndex] = x.length
+    }
+    
     for (i = 0; i < x.length; i++) 
     {
         x[i].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) 
-    {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    dots[slideIndex[slideNumber, slideshowIndex]].className += " active";
-    
-    x[slideIndex[slideshowIndex]-1].style.display = "block";  
+
+    // console.log(x)
+    // console.log("slideIndex:", slideIndex)
+    // console.log("slideshowIndex", slideshowIndex)
+    // console.log("slideIndex[slideshowIndex]", slideIndex[slideshowIndex])
+
+    x[slideIndex[slideshowIndex]-1].style.display = "block";
 }

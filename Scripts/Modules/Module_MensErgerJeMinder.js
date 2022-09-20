a7489db011d119a2b60441029b968095c838168c
xPeach-94 <sabrinahermans@live.nl>
2022-09-20 13:42:51 +0200

    today's update of mens erger je minder

--- Scripts/Modules/Module_MensErgerJeMinder.js
+++ Scripts/Modules/Module_MensErgerJeMinder.js
@@ -1,6 +1,16 @@
+let rightArrow = "&#10163";
+
 let playground = document.getElementById("mensErgerJeMinder");
 playground.classList.add("flex");
 playground.classList.add("vertical");
+
+// let testcontainer = document.createElement("div");
+// testcontainer.classList.add("test");
+// let test = document.createElement("p");
+// test.innerHTML = rightArrow;
+// test.style.fontSize = "70px"
+// testcontainer.appendChild(test);
+// playground.appendChild(testcontainer);
 
 const pawn = "&#9823;";
 
@@ -26,7 +36,40 @@ const createStartup = function()
             td.classList.add("flex");
             let div = document.createElement("div");
             div.id = "step"+uID;
+            if (uID == 5 || uID == 15 || uID == 35 || uID == 45) // adding arrows to starting positions
+            {
+                let startArrow = document.createElement("p");
+                startArrow.innerHTML = rightArrow;
+                startArrow.classList.add("startArrow")
+
+                switch (uID) 
+                {
+                    case 5:
+                    {
+                        startArrow.classList.add("greenArrow");
+                        break;
+                    }
+                    case 35:
+                    {
+                        startArrow.classList.add("redArrow");
+                        break;
+                    }
+                    case 45:
+                    {
+                        startArrow.classList.add("blueArrow");
+                        break;
+                    }
+                        
+                
+                    default:
+                        console.log("something went wrong :(", uID);
+                        break;
+                }
+                
+                div.appendChild(startArrow);
+            }
             div.classList.add("circle");
+            div.classList.add("flex");
 
             td.appendChild(div);
             tr.appendChild(td);
